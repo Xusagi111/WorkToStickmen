89110c8294c2f383bdf8095aa4c6c37265f5d6ff
Xusagi <xusagi.c.y@mail.ru>
2022-04-23 12:27:55 +0300

    Added Changes Movment

--- Assets/Code/MovmentPlayer.cs
+++ Assets/Code/MovmentPlayer.cs
@@ -7,12 +7,40 @@ public class MovmentPlayer : MonoBehaviour
     [SerializeField] DynamicJoystick _joystick;
     [SerializeField] Rigidbody _rigidbody;
     [SerializeField] float _moveSpeed;
+    [SerializeField] float Quaternion1;
+    [SerializeField] float Quaternion2;
+    [SerializeField] bool start = false;
     private void FixedUpdate()
     {
-        if (_joystick.Horizontal !=0 && _joystick.Vertical != 0 )
+        if (_joystick.Horizontal != 0 && _joystick.Vertical != 0)
         {
             _rigidbody.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _joystick.Vertical * _moveSpeed, 0);
+            Debug.Log($"_joystick.Horizontal: {_joystick.Horizontal} + _joystick.Vertical: {_joystick.Vertical} ");
+            if (!start)
+            {
+                StartCoroutine(NewTestCorrutine());
+            }
         }
+        else
+        {
+            _rigidbody.velocity = new Vector3(0, 0, 0);
+            _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(0, 0, 0));
+            Debug.Log("намскемхе");
+        }
+    }
+        IEnumerator NewTestCorrutine()
+    {
+        start = true;      
+        yield return new WaitForSeconds(0.1f);
+        if (_joystick.Horizontal > 0.6f)
+        {
 
+            _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(0, 0, Quaternion1));
+        }
+        if (_joystick.Horizontal < 0.6f)
+        {
+            _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(0, 0, Quaternion2));
+        }
+        start = false;
     }
 }
