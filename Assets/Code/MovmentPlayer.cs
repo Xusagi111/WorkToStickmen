493ee0d67f57c6abfa79b7c5ab391bd30bb91f02
Xusagi <xusagi.c.y@mail.ru>
2022-04-26 13:35:11 +0300

    Add

--- Assets/Code/MovmentPlayer.cs
+++ Assets/Code/MovmentPlayer.cs
@@ -22,54 +22,74 @@ public class MovmentPlayer : MonoBehaviour
 
     private void Update()
     {
-        _animator.SetBool("isBland", true);
-        _animator.SetFloat("Vertical", _joystick.Vertical * _moveSpeed);
-        _animator.SetFloat("Horizontal", _joystick.Horizontal * _moveSpeed);
-
         if (_joystick.Horizontal != 0 && _joystick.Vertical != 0)
         {
             Vector3 a = new Vector3(0, _joystick.Vertical * _moveSpeed, _joystick.Horizontal * _moveSpeed);
 
             _rigidbody.velocity = a;
             Debug.Log($"_joystick.Horizontal: {_joystick.Horizontal} + _joystick.Vertical: {_joystick.Vertical} ");
-
-
-            if (gameObject.transform.transform.eulerAngles.y > -_constRotate && _joystick.Horizontal < -0.1 && !_isRotateLeft)
-            {
-                gameObject.transform.transform.eulerAngles = new Vector3(0, -180, 0);
-                _isRotateLeft = true;
-                _isRotateRight = false;
-            }
-            if (gameObject.transform.transform.eulerAngles.y > _constRotate && _joystick.Horizontal > 0.0002 && !_isRotateRight)
-            {
-                gameObject.transform.transform.eulerAngles = new Vector3(0,0,0);
-                _isRotateLeft = false;
-                _isRotateRight = true;
-            }
-
-            //if (!start)
-            //{
-            //    StartCoroutine(NewTestCorrutine());
-            //}
-        }
-        else
-        {
-            //_rigidbody.velocity = new Vector3(0, 0, 0);
-            //_rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(0, 0, 0));
-            _animator.SetBool("isBland", false);
-            //Debug.Log("намскемхе");
         }
     }
-    IEnumerator NewTestCorrutine()
-    {
-        start = true;
-        yield return new WaitForSeconds(0.2f);
 
-        _animator.SetBool("isBland", true);
-        _animator.SetFloat("Vertical", _joystick.Vertical *_moveSpeed);
-        _animator.SetFloat("Horizontal", _joystick.Horizontal * _moveSpeed);
-        start = false;
-    }
+    //private void Update()
+    //{
+    //    if (_isRotateRight)
+    //    {
+    //        _animator.SetBool("isBland", true);
+    //        _animator.SetFloat("Vertical", -_joystick.Vertical * _moveSpeed);
+    //        _animator.SetFloat("Horizontal", -_joystick.Horizontal * _moveSpeed);
+    //    }
+    //    else
+    //    {
+    //        _animator.SetBool("isBland", true);
+    //        _animator.SetFloat("Vertical", _joystick.Vertical * _moveSpeed);
+    //        _animator.SetFloat("Horizontal", _joystick.Horizontal * _moveSpeed);
+    //    }
+
+    //    if (_joystick.Horizontal != 0 && _joystick.Vertical != 0)
+    //    {
+    //        //Vector3 a = new Vector3(0, _joystick.Vertical * _moveSpeed, _joystick.Horizontal * _moveSpeed);
+
+    //        //_rigidbody.velocity = a;
+    //        Debug.Log($"_joystick.Horizontal: {_joystick.Horizontal} + _joystick.Vertical: {_joystick.Vertical} ");
+
+
+    //        if (gameObject.transform.transform.eulerAngles.y > -_constRotate && _joystick.Horizontal < -0.1 && !_isRotateLeft)
+    //        {
+    //            gameObject.transform.transform.eulerAngles = new Vector3(0, -180, 0);
+    //            _isRotateLeft = true;
+    //            _isRotateRight = false;
+    //        }
+    //        if (gameObject.transform.transform.eulerAngles.y > _constRotate && _joystick.Horizontal > 0.0002 && !_isRotateRight)
+    //        {
+    //            gameObject.transform.transform.eulerAngles = new Vector3(0,0,0);
+    //            _isRotateLeft = false;
+    //            _isRotateRight = true;
+    //        }
+
+    //        //if (!start)
+    //        //{
+    //        //    StartCoroutine(NewTestCorrutine());
+    //        //}
+    //    }
+    //    else
+    //    {
+    //        //_rigidbody.velocity = new Vector3(0, 0, 0);
+    //        //_rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(0, 0, 0));
+    //        _animator.SetBool("isBland", false);
+    //        //Debug.Log("намскемхе");
+    //    }
+    //}
+    //IEnumerator NewTestCorrutine()
+    //{
+    //    start = true;
+    //    yield return new WaitForSeconds(0.2f);
+
+    //    _animator.SetBool("isBland", true);
+    //    _animator.SetFloat("Vertical", _joystick.Vertical *_moveSpeed * Time.deltaTime);
+    //    _animator.SetFloat("Horizontal", _joystick.Horizontal * _moveSpeed * Time.deltaTime);
+    //    start = false;
+    //}
 
     //IEnumerator NewTestCorrutine()
     //{
