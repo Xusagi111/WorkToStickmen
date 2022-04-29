79257156b11a2f71c5ac4858220d616deed194a2
Xusagi <xusagi.c.y@mail.ru>
2022-04-29 19:03:25 +0300

    Added fix Enemy + Player

--- Assets/Code/Link.cs
+++ Assets/Code/Link.cs
@@ -1,9 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using RootMotion.Dynamics;
+
 public class Link : MonoBehaviour
 {
     public PuppetMaster PuppetMaster;
-    public GameObject CurrentGameObj;
+    public GameObject HeadEnemy;
+    public Animator AnimatorEnemy;
 }
